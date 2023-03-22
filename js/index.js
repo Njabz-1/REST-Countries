98657fa5d112823c807ec255831a3213994d93c7
Njabulo <mhlambinjabulo@gmail.com>
2023-03-22 20:16:36 +0200

    Styling edits: Flag size made the same and removed border radius. Also cards are aligned now

--- js/index.js
+++ js/index.js
@@ -34,15 +34,23 @@ async function main() {
     const countries = await getCountries();
     console.log(countries);
     const countriesContainer = document.getElementById("countries");
+    const container = document.createElement("div");
+    container.className = "custom-container px-4";
+    const row = document.createElement("div");
+    row.className = "row g-5";
+    container.appendChild(row);
   
-    loadMoreCountries(countries, countriesContainer);
+    document.body.appendChild(container);
+  
+  
+    loadMoreCountries(countries, row);
   
     // Add the scroll event listener
     window.addEventListener("scroll", () => {
       if (window.innerHeight + window.pageYOffset >=
         document.documentElement.offsetHeight - 1) {
         currentPage++;
-        loadMoreCountries(countries, countriesContainer);
+        loadMoreCountries(countries, row);
       }
     });
   }
@@ -50,51 +58,60 @@ async function main() {
   main();
 
 //   Creating country card
-  function createCountryCard(country) {
+function createCountryCard(country) {
+    // Create a column element
+    const col = document.createElement("div");
+    col.className = "col-md-3 ms-auto";
+  
     const card = document.createElement('div');
     card.className = 'card';
-
+  
     const flag = document.createElement('img');
     flag.src = country.flags ? country.flags.png : '';
     flag.className = 'card-img-top';
-
+    flag.style.width = '100%'; // Adjust the width as needed
+    flag.style.height = '200px'; // Adjust the height as needed
+    flag.style.objectFit = 'cover';
+  
     const cardBody = document.createElement('div');
     cardBody.className = 'card-body';
-
+  
     const countryName = document.createElement('h5');
     countryName.className = 'card-title';
     countryName.textContent = country.name.common;
-
+  
     const countryRegion = document.createElement('p');
     countryRegion.className = 'card-text';
     countryRegion.textContent = `Region: ${country.region}`;
-
+  
     const countryCapital = document.createElement('p');
     countryCapital.className = 'card-text';
     countryCapital.textContent = `Capital: ${country.capital}`;
-
+  
     const countryPopulation = document.createElement('p');
     countryPopulation.className = 'card-text';
     countryPopulation.textContent = `Population: ${country.population}`;
-
+  
     cardBody.appendChild(countryName);
     cardBody.appendChild(countryPopulation);
     cardBody.appendChild(countryRegion);
     cardBody.appendChild(countryCapital);
-
-
+  
     card.appendChild(flag);
     card.appendChild(cardBody);
+  
+    // Append the card to the column element
+    col.appendChild(card);
+  
+    return col;
+  }
 
-    return card;
-}
-
-function loadMoreCountries(countries, countriesContainer) {
+function loadMoreCountries(countries, row) {
     const startIndex = (currentPage - 1) * countriesPerPage;
     const endIndex = startIndex + countriesPerPage;
   
     for (let i = startIndex; i < endIndex && i < countries.length; i++) {
       const countryCard = createCountryCard(countries[i]);
-      countriesContainer.appendChild(countryCard);
+      row.appendChild(countryCard);
     }
   }
