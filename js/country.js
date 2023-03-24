eba8764b0a8a062f11cf320f265396bd98049580
Njabulo <mhlambinjabulo@gmail.com>
2023-03-24 17:54:48 +0200

    Got the country codes to be onverted to country full name

--- js/country.js
+++ js/country.js
@@ -1,68 +1,104 @@
 // Toggling dark mode
-const darkModeToggle = document.getElementById('darkModeToggle');
-const darkModeText = document.querySelector('#darkModeToggle h4');
-const darkModeClass = 'dark-mode';
-const lightModeClass = 'light-mode';
-const modeIcon = document.getElementById('modeIcon');
-const sunIconSrc = './assets/images/icons/icons8-light-mode-78.png'; 
-const moonIconSrc = './assets/images/icons/icons8-crescent-moon-64.png';
+const darkModeToggle = document.getElementById("darkModeToggle");
+const darkModeText = document.querySelector("#darkModeToggle h4");
+const darkModeClass = "dark-mode";
+const lightModeClass = "light-mode";
+const modeIcon = document.getElementById("modeIcon");
+const sunIconSrc = "./assets/images/icons/icons8-light-mode-78.png";
+const moonIconSrc = "./assets/images/icons/icons8-crescent-moon-64.png";
 
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle(darkModeClass);
-    document.body.classList.toggle(lightModeClass);
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle(darkModeClass);
+  document.body.classList.toggle(lightModeClass);
 
-    if (document.body.classList.contains(darkModeClass)) {
-        modeIcon.src = sunIconSrc;
-        darkModeText.textContent = 'Light Mode';
-    } else {
-        modeIcon.src = moonIconSrc;
-        darkModeText.textContent = 'Dark Mode';
-    }
+  if (document.body.classList.contains(darkModeClass)) {
+    modeIcon.src = sunIconSrc;
+    darkModeText.textContent = "Light Mode";
+  } else {
+    modeIcon.src = moonIconSrc;
+    darkModeText.textContent = "Dark Mode";
+  }
 });
 
 // Back to home page
-document.getElementById('back-button').addEventListener('click', () => {
-    window.location.href = 'index.html';
-  });
+document.getElementById("back-button").addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
 // getting selected country via local storage
-const selectedCountry = JSON.parse(localStorage.getItem('selectedCountry'));
+const selectedCountry = JSON.parse(localStorage.getItem("selectedCountry"));
 
-// Update the DOM with the country data
-const countryNameEl = document.getElementById('country-name');
-const nativeNameEl = document.getElementById('native-name');
-const populationEl = document.getElementById('population');
-const region = document.getElementById('region');
-const subregion = document.getElementById('sub-region');
-const capital = document.getElementById('capital');
-const topLevelDomain = document.getElementById('tld');
-const currency = document.getElementById('currency');
-const languages = document.getElementById('languages');
-const flagImageURL = selectedCountry.flags.png;
-const flagImage = document.getElementById('country-flag');
-const bordersElement = document.getElementById('borders');
-
-countryNameEl.textContent = selectedCountry.name.common;
-nativeNameEl.textContent = Object.values(selectedCountry.name.nativeName)[0].common;
-populationEl.textContent = selectedCountry.population;
-region.textContent = selectedCountry.region;
-subregion.textContent = selectedCountry.subregion;
-capital.textContent = selectedCountry.capital[0];
-topLevelDomain.textContent = selectedCountry.tld.join(", ");
-// Currency is more complicated 
-const currencyCode = Object.keys(selectedCountry.currencies)[0]; // assumes there is only one currency
-const currencyName = selectedCountry.currencies[currencyCode].name;
-currency.textContent = currencyName;
-languages.textContent = Object.values(selectedCountry.languages).join(", ");
-flagImage.src = flagImageURL;
-// Borders
-function createBorderCountrySpan(countryCode) {
-    const span = document.createElement('span');
-    span.classList.add('border-country');
-    span.textContent = countryCode;
-    return span;
+async function getCountries() {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching countries:", error);
+    return [];
   }
+}
+let countries = [];
+
+async function main() {
+  countries = await getCountries();
+  // Update the DOM with the country data
+  const countryNameEl = document.getElementById("country-name");
+  const nativeNameEl = document.getElementById("native-name");
+  const populationEl = document.getElementById("population");
+  const region = document.getElementById("region");
+  const subregion = document.getElementById("sub-region");
+  const capital = document.getElementById("capital");
+  const topLevelDomain = document.getElementById("tld");
+  const currency = document.getElementById("currency");
+  const languages = document.getElementById("languages");
+  const flagImageURL = selectedCountry.flags.png;
+  const flagImage = document.getElementById("country-flag");
+  const bordersElement = document.getElementById("borders");
+
+  countryNameEl.textContent = selectedCountry.name.common;
+  nativeNameEl.textContent = Object.values(
+    selectedCountry.name.nativeName
+  )[0].common;
+  populationEl.textContent = selectedCountry.population;
+  region.textContent = selectedCountry.region;
+  subregion.textContent = selectedCountry.subregion;
+  capital.textContent = selectedCountry.capital[0];
+  topLevelDomain.textContent = selectedCountry.tld.join(", ");
+
+  // Currency is more complicated
+  const currencyCode = Object.keys(selectedCountry.currencies)[0]; // assumes there is only one currency
+  const currencyName = selectedCountry.currencies[currencyCode].name;
+  currency.textContent = currencyName;
+
+  languages.textContent = Object.values(selectedCountry.languages).join(", ");
+  flagImage.src = flagImageURL;
+
+  // Borders
+  function createBorderCountrySpan(borderCountryCode) {
+    const borderCountry = countries.find(
+      (country) => country.cca3 === borderCountryCode
+    );
+
+    // Check if the border country is found
+    if (borderCountry) {
+      const span = document.createElement("span");
+      span.classList.add("border-country");
+      span.textContent = borderCountry.name.common;
+      return span;
+    } else {
+      console.warn(`Country with cca3 '${borderCountryCode}' not found.`);
+      return null;
+    }
+  }
+
   selectedCountry.borders.forEach((borderCountryCode) => {
-    const borderCountrySpan = createBorderCountrySpan(borderCountryCode);
+    const borderCountrySpan = createBorderCountrySpan(
+      borderCountryCode,
+      countries
+    );
     bordersElement.appendChild(borderCountrySpan);
   });
-console.log(selectedCountry);
+}
+
+main();
--- js/index.js
+++ js/index.js
@@ -1,43 +1,44 @@
 // Toggling dark mode
 
-const darkModeToggle = document.getElementById('darkModeToggle');
-const darkModeText = document.querySelector('#darkModeToggle h4');
-const darkModeClass = 'dark-mode';
-const lightModeClass = 'light-mode';
-const modeIcon = document.getElementById('modeIcon');
-const sunIconSrc = './assets/images/icons/icons8-light-mode-78.png'; 
-const moonIconSrc = './assets/images/icons/icons8-crescent-moon-64.png';
+const darkModeToggle = document.getElementById("darkModeToggle");
+const darkModeText = document.querySelector("#darkModeToggle h4");
+const darkModeClass = "dark-mode";
+const lightModeClass = "light-mode";
+const modeIcon = document.getElementById("modeIcon");
+const sunIconSrc = "./assets/images/icons/icons8-light-mode-78.png";
+const moonIconSrc = "./assets/images/icons/icons8-crescent-moon-64.png";
 
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle(darkModeClass);
-    document.body.classList.toggle(lightModeClass);
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle(darkModeClass);
+  document.body.classList.toggle(lightModeClass);
 
-    if (document.body.classList.contains(darkModeClass)) {
-        modeIcon.src = sunIconSrc;
-        darkModeText.textContent = 'Light Mode';
-    } else {
-        modeIcon.src = moonIconSrc;
-        darkModeText.textContent = 'Dark Mode';
-    }
+  if (document.body.classList.contains(darkModeClass)) {
+    modeIcon.src = sunIconSrc;
+    darkModeText.textContent = "Light Mode";
+  } else {
+    modeIcon.src = moonIconSrc;
+    darkModeText.textContent = "Dark Mode";
+  }
 });
 
 // Fetch request of all countries
-async function getCountries(){
-    try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error("Error fetching countries:", error);
-        return [];
-      }
+async function getCountries() {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching countries:", error);
+    return [];
+  }
 }
 let currentPage = 1;
 const countriesPerPage = 8;
 
 async function main() {
-    const countries = await getCountries();
-    countries.sort((a, b) => {
+  const countries = await getCountries();
+  countries
+    .sort((a, b) => {
       if (a.name.common < b.name.common) {
         return 1;
       }
@@ -45,96 +46,97 @@ async function main() {
         return -1;
       }
       return 0;
-    }).reverse();
-    console.log(countries);
-    const countriesContainer = document.getElementById("countries");
-    const container = document.createElement("div");
-    container.className = "custom-container px-4";
-    const row = document.createElement("div");
-    row.className = "row g-5";
-    container.appendChild(row);
-  
-    countriesContainer.appendChild(container);
-  
-  
-    loadMoreCountries(countries, row);
-  
-    // Add the scroll event listener
-    window.addEventListener("scroll", () => {
-      if (window.innerHeight + window.scrollY >=
-        document.documentElement.offsetHeight - 1) {
-        currentPage++;
-        loadMoreCountries(countries, row);
-      }
-    });
-  }
-  
-  main();
+    })
+    .reverse();
+  console.log(countries);
+  const countriesContainer = document.getElementById("countries");
+  const container = document.createElement("div");
+  container.className = "custom-container px-4";
+  const row = document.createElement("div");
+  row.className = "row g-5";
+  container.appendChild(row);
+
+  countriesContainer.appendChild(container);
+
+  loadMoreCountries(countries, row);
+
+  // Add the scroll event listener
+  window.addEventListener("scroll", () => {
+    if (
+      window.innerHeight + window.scrollY >=
+      document.documentElement.offsetHeight - 1
+    ) {
+      currentPage++;
+      loadMoreCountries(countries, row);
+    }
+  });
+}
+
+main();
 
 //   Creating country card
 function createCountryCard(country) {
-    // Create a column element
-    const col = document.createElement("div");
-    col.className = "col-md-3 ms-auto";
-  
-    const card = document.createElement('div');
-    card.className = 'card';
-  
-    const flag = document.createElement('img');
-    flag.src = country.flags ? country.flags.png : '';
-    flag.className = 'card-img-top';
-    flag.style.width = '100%'; // Adjust the width as needed
-    flag.style.height = '200px'; // Adjust the height as needed
-    flag.style.objectFit = 'cover';
-  
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-  
-    const countryName = document.createElement('h5');
-    countryName.className = 'card-title';
-    countryName.textContent = country.name.common;
-  
-    const countryRegion = document.createElement('p');
-    countryRegion.className = 'card-text';
-    countryRegion.textContent = `Region: ${country.region}`;
-  
-    const countryCapital = document.createElement('p');
-    countryCapital.className = 'card-text';
-    countryCapital.textContent = `Capital: ${country.capital}`;
-  
-    const countryPopulation = document.createElement('p');
-    countryPopulation.className = 'card-text';
-    countryPopulation.textContent = `Population: ${country.population}`;
-  
-    cardBody.appendChild(countryName);
-    cardBody.appendChild(countryPopulation);
-    cardBody.appendChild(countryRegion);
-    cardBody.appendChild(countryCapital);
-  
-    card.appendChild(flag);
-    card.appendChild(cardBody);
-  
-    // Append the card to the column element
-    col.appendChild(card);
+  // Create a column element
+  const col = document.createElement("div");
+  col.className = "col-md-3 ms-auto";
 
-    // Add click event listener to card element
-    card.addEventListener('click', () => {
-      // Save the selected country data in the local storage
-      localStorage.setItem('selectedCountry', JSON.stringify(country));
-      // Redirect to the country.html page
-      window.location.href = 'country.html';
-    });
-    console.log(localStorage.getItem('selectedCountry'));
-    return col;
-    
-  }
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const flag = document.createElement("img");
+  flag.src = country.flags ? country.flags.png : "";
+  flag.className = "card-img-top";
+  flag.style.width = "100%"; // Adjust the width as needed
+  flag.style.height = "200px"; // Adjust the height as needed
+  flag.style.objectFit = "cover";
+
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+
+  const countryName = document.createElement("h5");
+  countryName.className = "card-title";
+  countryName.textContent = country.name.common;
+
+  const countryRegion = document.createElement("p");
+  countryRegion.className = "card-text";
+  countryRegion.textContent = `Region: ${country.region}`;
+
+  const countryCapital = document.createElement("p");
+  countryCapital.className = "card-text";
+  countryCapital.textContent = `Capital: ${country.capital}`;
+
+  const countryPopulation = document.createElement("p");
+  countryPopulation.className = "card-text";
+  countryPopulation.textContent = `Population: ${country.population}`;
+
+  cardBody.appendChild(countryName);
+  cardBody.appendChild(countryPopulation);
+  cardBody.appendChild(countryRegion);
+  cardBody.appendChild(countryCapital);
+
+  card.appendChild(flag);
+  card.appendChild(cardBody);
+
+  // Append the card to the column element
+  col.appendChild(card);
+
+  // Add click event listener to card element
+  card.addEventListener("click", () => {
+    // Save the selected country data in the local storage
+    localStorage.setItem("selectedCountry", JSON.stringify(country));
+    // Redirect to the country.html page
+    window.location.href = "country.html";
+  });
+  console.log(localStorage.getItem("selectedCountry"));
+  return col;
+}
 
 function loadMoreCountries(countries, row) {
-    const startIndex = (currentPage - 1) * countriesPerPage;
-    const endIndex = startIndex + countriesPerPage;
-  
-    for (let i = startIndex; i < endIndex && i < countries.length; i++) {
-      const countryCard = createCountryCard(countries[i]);
-      row.appendChild(countryCard);
-    }
+  const startIndex = (currentPage - 1) * countriesPerPage;
+  const endIndex = startIndex + countriesPerPage;
+
+  for (let i = startIndex; i < endIndex && i < countries.length; i++) {
+    const countryCard = createCountryCard(countries[i]);
+    row.appendChild(countryCard);
   }
+}
